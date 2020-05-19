e698e759879e268ddb3cec77644866093c186d89
Ashish Verma <ashishverma.mail@gmail.com>
2020-05-19 17:29:26 +0530

    Initial Commit

---
+++ SwiftUIBeginnerTests/SwiftUIBeginnerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIBeginnerTests.swift
+//  SwiftUIBeginnerTests
+//
+//  Created by Ashish Verma on 5/19/20.
+//  Copyright © 2020 Ashish Verma. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUIBeginner
+
+class SwiftUIBeginnerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
